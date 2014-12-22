1046840cc9580e0ef2378c49cf85720c2d224366
Frankie Dintino <fdintino@theatlantic.com>
2014-12-22 08:59:57 -0500

    Fix bugs with char/word limit snapshot implementation
    The issue was caused by a delay between the firing of the
    snapshot event relative to when the snapshot was last saved
    (the contents of which were used in "undo-ing" the typing.
    It was possible to exceed the character limit when typing
    quickly or copy-pasting quickly.
    
    From callahat/CKEditor-WordCount-Plugin@6118523

--- wordcount/plugin.js
+++ wordcount/plugin.js
@@ -9,10 +9,11 @@ CKEDITOR.plugins.add('wordcount', {
     init: function(editor) {
         var defaultFormat = '',
             intervalId,
-            lastWordCount,
-            lastCharCount = 0,
+            lastWordCount = -1,
+            lastCharCount = -1,
             limitReachedNotified = false,
-            limitRestoredNotified = false;
+            limitRestoredNotified = false,
+            snapShot = editor.getSnapshot();
 
         // Default Config
         var defaultConfig = {
@@ -81,129 +82,149 @@ CKEDITOR.plugins.add('wordcount', {
             return tmp.textContent || tmp.innerText;
         }
 
+        function countCharacters(text){
+            if (config.countHTML) {
+                return( text.length );
+            } else {
+                // strip body tags
+                if (editor.config.fullPage) {
+                    var i = text.search(new RegExp("<body>", "i"));
+                    if (i != -1) {
+                        var j = text.search(new RegExp("</body>", "i"));
+                        text = text.substring(i + 6, j);
+                    }
+
+                }
+
+                var normalizedText = text.
+                    replace(/(\r\n|\n|\r)/gm, "").
+                    replace(/^\s+|\s+$/g, "").
+                    replace("&nbsp;", "");
+
+                if (!config.countSpacesAsChars) {
+                    normalizedText = text.
+                        replace(/\s/g, "");
+                }
+
+                normalizedText = strip(normalizedText).replace(/^([\s\t\r\n]*)$/, "");
+
+                return( normalizedText.length );
+            }
+        }
+
+        function countWords(text){
+            var normalizedText = text.
+                replace(/(\r\n|\n|\r)/gm, " ").
+                replace(/^\s+|\s+$/g, "").
+                replace("&nbsp;", " ");
+
+            normalizedText = strip(normalizedText);
+
+            var words = normalizedText.split(/\s+/);
+
+            for (var wordIndex = words.length - 1; wordIndex >= 0; wordIndex--) {
+                if (words[wordIndex].match(/^([\s\t\r\n]*)$/)) {
+                    words.splice(wordIndex, 1);
+                }
+            }
+
+            return( words.length );
+        }
+
         function updateCounter(editorInstance) {
-            var paragraphs = 0,
-                wordCount = 0,
+            var wordCount = 0,
                 charCount = 0,
-                normalizedText,
+                paragraphs = 0,
+                deltaChar,
+                deltaWord,
                 text;
 
             if (text = editorInstance.getData()) {
                 if (config.showCharCount) {
-                    if (config.countHTML) {
-                        charCount = text.length;
-                    } else {
-                        // strip body tags
-                        if (editor.config.fullPage) {
-                            var i = text.search(new RegExp("<body>", "i"));
-                            if (i != -1) {
-                                var j = text.search(new RegExp("</body>", "i"));
-                                text = text.substring(i + 6, j);
-                            }
-
-                        }
-
-                        normalizedText = text.
-                            replace(/(\r\n|\n|\r)/gm, "").
-                            replace(/^\s+|\s+$/g, "").
-                            replace("&nbsp;", "");
-
-                        if (!config.countSpacesAsChars) {
-                            normalizedText = text.
-                                replace(/\s/g, "");
-                        }
-
-                        normalizedText = strip(normalizedText).replace(/^([\s\t\r\n]*)$/, "");
-
-                        charCount = normalizedText.length;
-                    }
-                }
-
-                if (config.showParagraphs) {
-                    paragraphs = text.replace(/&nbsp;/g, " ").replace(/(<([^>]+)>)/ig, "").replace(/^\s*$[\n\r]{1,}/gm, "++").split("++").length;
+                    charCount = countCharacters(text);
                 }
 
                 if (config.showWordCount) {
-                    normalizedText = text.
-                        replace(/(\r\n|\n|\r)/gm, " ").
-                        replace(/^\s+|\s+$/g, "").
-                        replace("&nbsp;", " ");
-
-                    normalizedText = strip(normalizedText);
-
-                    var words = normalizedText.split(/\s+/);
-
-                    for (var wordIndex = words.length - 1; wordIndex >= 0; wordIndex--) {
-                        if (words[wordIndex].match(/^([\s\t\r\n]*)$/)) {
-                            words.splice(wordIndex, 1);
-                        }
-                    }
-
-                    wordCount = words.length;
+                    wordCount = countWords(text);
                 }
 
-                var html = format.replace('%wordCount%', wordCount).replace('%charCount%', charCount).replace('%paragraphs%', paragraphs);
-
-                editorInstance.plugins.wordcount.wordCount = wordCount;
-                editorInstance.plugins.wordcount.charCount = charCount;
-
-                if (CKEDITOR.env.gecko) {
-                    counterElement(editorInstance).innerHTML = html;
-                } else {
-                    counterElement(editorInstance).innerText = html;
+                if (config.showParagraphs) {
+                    paragraphs = text.replace(/&nbsp;/g, " ")
+                        .replace(/(<([^>]+)>)/ig, "")
+                        .replace(/^\s*$[\n\r]{1,}/gm, "++")
+                        .split("++").length;
                 }
-
-                if (charCount == lastCharCount) {
-                    return true;
-                }
-
-                lastWordCount = wordCount;
-                lastCharCount = charCount;
-
-                // Check for word limit
-                if (config.showWordCount && wordCount > config.wordLimit) {
-                    limitReached(editor, limitReachedNotified);
-                } else if (config.showWordCount && wordCount == config.wordLimit) {
-                    // create snapshot to make sure only the content after the limit gets deleted
-                    editorInstance.fire('saveSnapshot');
-                } else if (!limitRestoredNotified && wordCount < config.wordLimit) {
-                    limitRestored(editor);
-                }
-
-                // Check for char limit
-                if (config.showCharCount && charCount > config.charLimit) {
-                       limitReached(editor, limitReachedNotified);
-                   } else if (config.showCharCount && charCount == config.charLimit) {
-                       // create snapshot to make sure only the content after the limit gets deleted
-                       editorInstance.fire('saveSnapshot');
-                   } else if (!limitRestoredNotified && charCount < config.charLimit) {
-                       limitRestored(editor);
-                   }
             }
 
+            var html = format.replace('%wordCount%', wordCount).replace('%charCount%', charCount).replace('%paragraphs%', paragraphs);
+
+            editorInstance.plugins.wordcount.wordCount = wordCount;
+            editorInstance.plugins.wordcount.charCount = charCount;
+
+            if (CKEDITOR.env.gecko) {
+                counterElement(editorInstance).innerHTML = html;
+            } else {
+                counterElement(editorInstance).innerText = html;
+            }
+
+            if (charCount == lastCharCount && wordCount == lastWordCount) {
+                return true;
+            }
+
+            //If the limit is already over, allow the deletion of characters/words. Otherwise,
+            //the user would have to delete at one go the number of offending characters
+            deltaWord = wordCount - lastWordCount;
+            deltaChar = charCount - lastCharCount;
+
+            lastWordCount = wordCount;
+            lastCharCount = charCount;
+
+            if(lastWordCount == -1){
+                lastWordCount = wordCount;
+            }
+            if(lastCharCount == -1){
+                lastCharCount = charCount;
+            }
+
+            // Check for word limit and/or char limit
+            if ((config.wordLimit != "unlimited" && wordCount > config.wordLimit && deltaWord > 0) ||
+                (config.charLimit != "unlimited" && charCount > config.charLimit && deltaChar > 0)) {
+                limitReached(editorInstance, limitReachedNotified);
+            } else if (!limitRestoredNotified && 
+                        (config.wordLimit == "unlimited" || wordCount < config.wordLimit) &&
+                        (config.charLimit == "unlimited" || charCount < config.charLimit) ) {
+                limitRestored(editorInstance);
+            } else {
+                snapShot = editorInstance.getSnapshot();
+            }
 
             return true;
         }
 
         function limitReached(editorInstance, notify) {
-             limitReachedNotified = true;
-             limitRestoredNotified = false;
-             editorInstance.execCommand('undo');
-             if (!notify) {
-                counterElement(editorInstance).className = "cke_wordcount cke_wordcountLimitReached";
-                editorInstance.fire('limitReached', {}, editor);
-             }
-             // lock editor
-             editorInstance.config.Locked = 1;
-         }
+            limitReachedNotified = true;
+            limitRestoredNotified = false;
 
-         function limitRestored(editorInstance) {
-             limitRestoredNotified = true;
-             limitReachedNotified = false;
-             counterElement(editorInstance).className = "cke_wordcount";
-             // unlock editor
-             editorInstance.config.Locked = 0;
-         }
+            editorInstance.loadSnapshot(snapShot);
+
+            if (!notify) {
+               counterElement(editorInstance).className = "cke_wordcount cke_wordcountLimitReached";
+               editorInstance.fire('limitReached', {}, editor);
+            }
+            
+            // lock editor
+            editorInstance.config.Locked = 1;
+        }
+
+        function limitRestored(editorInstance) {
+            
+            limitRestoredNotified = true;
+            limitReachedNotified = false;
+            editorInstance.config.Locked = 0;
+            snapShot = editor.getSnapshot();
+            
+            counterElement(editorInstance).className = "cke_wordcount";
+        }
 
         editor.on('key', function(event) {
             if (editor.mode === 'source') {
